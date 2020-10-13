58e6068987f055a29f857b06f1a91a936352f47f
jodurpar <jose.duran@duransoftware.es>
2020-10-13 12:23:44 +0200

    Add docker folder content

---
+++ DSDeviceDetection/docker/swagger/swaggerdoc.js
@@ -0,0 +1,41 @@
+﻿/**
+ * @swagger
+ * /DeviceDetect:
+ *   get:
+ *     summary: Dectecting device from user agent.
+ *     description: Parse user agent and return a json properties.
+ *     parameters:
+ *      - name: useragent
+ *        description : the useragent.
+ *        in : query
+ *        required : true       
+ *        type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Device from user agent.
+ *       500: 
+ *          description: Internal server error.
+ */
+
+/**
+ * @swagger
+ * /DeviceDetectAsync:
+ *   get:
+ *     summary: Dectecting device from user agent (Async).
+ *     description: Parse user agent and return a json properties.
+ *     parameters:
+ *      - name: useragent
+ *        description : the useragent.
+ *        in : query
+ *        required : true       
+ *        type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Device from user agent.
+ *       500: 
+ *          description: Internal server error.
+ */
